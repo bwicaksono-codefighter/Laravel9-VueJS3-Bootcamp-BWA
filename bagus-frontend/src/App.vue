bf645b6bf15615a7c58205a8fa7082d7a2bf44d9
bwicaksono-codefighter <baguswicak@gmail.com>
2022-11-25 11:28:50 +0700

    Navbar

--- bagus-frontend/src/App.vue
+++ bagus-frontend/src/App.vue
@@ -39,32 +39,32 @@ import { RouterLink, RouterView } from 'vue-router'
             class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-regular"
           >
             <li>
-              <a
-                href="#"
+              <RouterLink
+                to="/"
                 class="block py-2 pl-3 pr-4 text-white bg-indigo-600 rounded md:bg-transparent md:text-indigo-600 md:p-0 dark:text-white"
                 aria-current="page"
-                >Home</a
+                >Home</RouterLink
               >
             </li>
             <li>
-              <a
-                href="#"
+              <RouterLink
+                to="/categories"
                 class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >Categories</a
+                >Categories</RouterLink
               >
             </li>
             <li>
-              <a
-                href="#"
+              <RouterLink
+                to="/pricing"
                 class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >Pricing</a
-              >
+                >Pricing</RouterLink
+              >              
             </li>
             <li>
-              <a
-                href="#"
+              <RouterLink
+                to="#"
                 class="block py-2 pl-3 pr-4 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-indigo-600 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >Study Case</a
+                >Study Case</RouterLink
               >
             </li>
           </ul>
